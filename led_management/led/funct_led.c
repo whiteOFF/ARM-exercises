79bd23135ea88bb258c098b73c66c198a0e06bc8
whiteOFF <89868763+whiteOFF@users.noreply.github.com>
2023-12-12 23:31:24 +0100

    readability updating

--- led_management/led/funct_led.c
+++ led_management/led/funct_led.c
@@ -12,7 +12,7 @@ void led4_off(void) {
 
 void ledevenon_oddoff(void) {
 	LPC_GPIO2 -> FIOSET |= 0x55;
-	LPC_GPIO2 -> FIOSET &= 0x55;	
+	LPC_GPIO2 -> FIOSET &= 0x55; // update state
 	LPC_GPIO2 -> FIOCLR &= 0xAA;
 }
 
@@ -24,4 +24,7 @@ void led_on(unsigned int n) {
 void led_off(unsigned int n) {
 	unsigned int pos = 1 << (11-n);
 	LPC_GPIO2 -> FIOCLR |= pos;
+	unsigned int pos_res = 0xFFFFFFFE;
+	pos_res = (pos_res << (11-n)) | (pos_res >> (32-11+n)); // rotate left
+	LPC_GPIO2 -> FIOSET &= pos_res; // update state
 }
